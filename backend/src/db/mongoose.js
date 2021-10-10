7f82a8c6ffcc64782dfea1b1ac8d26d71b13854f
mero <arsmohamed@outlook.com>
2021-10-10 12:18:11 -0230

    fixed credintional prob to connect to the DB

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = 'mongodb+srv://mero:AmAm1234@notask.ydi4i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const MONGODB_URI = 'mongodb+srv://mero:Am1234@notask.ydi4i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 mongoose.connect(MONGODB_URI || 'mongodb://127.0.0.1:27017/notask-Test', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 })
